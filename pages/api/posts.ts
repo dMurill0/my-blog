1a9889ff9fcb3c8d184238c306cd626fe3faee19
Daniel Murillo <sittingbout@gmail.com>
2023-01-11 12:17:07 +0100

    creado formulario de posts

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -2,9 +2,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-await fetch("")
+    await fetch("http://localhost:3001/items", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: req.body,
+    });
 
-    return res.status(201).json({hola: "mundo"})
+    return res.status(201).json({ hola: "mundo" });
   }
 
   return res.status(200).json({ hola: "mundo" });
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,16 +1,20 @@
-import React, { useState } from "react";
+import React, { SyntheticEvent, useState } from "react";
 
 type Props = {};
 
 function Index({}: Props) {
   const [fecha, setFecha] = useState("");
 
-  function submit() {
+  function submit(e: SyntheticEvent) {
+    e.preventDefault();
     const date = new Date();
-    console.log(date.toLocaleDateString());
     setFecha(date.toLocaleDateString());
+    const formData = {
+      title: e.target.title.value,
+      content: e.target.content.value,
+    };
+    console.log(formData);
   }
-  
 
   return (
     <div className="min-h-screen w-min-screen h-full w-full bg-slate-400">
@@ -18,13 +22,20 @@ function Index({}: Props) {
         className="flex flex-col w-1/3 h-fit space-y-12 p-10 mx-auto"
         action=""
         method="post"
+        onSubmit={submit}
       >
         <h1>Post:</h1>
         <h2>{fecha}</h2>
-        <input type="text" name="" id="" />
-        <input type="text" name="" id="" />
-        <textarea name="content" id=""></textarea>
-        <button className="px-4 py-2 bg-slate-600 rounded-full w-1/6 mx-auto text-white" type="submit">Enviar</button>
+        <h1>Título</h1>
+        <input type="text" name="title" id="" />
+        {/* <input type="text" name="" id="" /> */}
+        <h1>Texto</h1>
+        <textarea name="content" id="" />
+        <input
+          className="px-4 py-2 bg-slate-600 rounded-full w-1/6 mx-auto text-white"
+          type="submit"
+          value="Enviar"
+        />
       </form>
     </div>
   );
